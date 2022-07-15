ed5d5e0cc2ae1ab85ed7d214993adaed11f58c4d
juan pico <juan.pico-m@mail.escuelaing.edu.co>
2022-07-14 21:58:19 -0500

    Implementando chat 1.0.2

--- src/main/java/co/edu/escuelaing/interactiveblackboard/controllers/ChatController.java
+++ src/main/java/co/edu/escuelaing/interactiveblackboard/controllers/ChatController.java
@@ -1,8 +1,29 @@
 package co.edu.escuelaing.interactiveblackboard.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.converter.SimpleMessageConverter;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import co.edu.escuelaing.interactiveblackboard.entities.Message;
 
 @Controller
 public class ChatController {
-    
+
+    @Autowired
+    private SimpMessagingTemplate SimpMessagingTemplate;
+    // Un broadcast para que las demás personas lean el mensaje 
+    @MessageMapping("/message") //app/message "this url"
+    @SendTo("chatroom/public")
+    private Message receivePublicMessage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("private-message")
+    public Message receiverPrivateMessage(@Payload Message message){
+        return message;
+    }
+
 }
