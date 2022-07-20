9b9ff1c668b5c18e715ecd3e824428845f1644de
juan pico <juan.pico-m@mail.escuelaing.edu.co>
2022-07-19 22:53:15 -0500

    Borrando Websocket completo

--- src/main/java/co/edu/escuelaing/interactiveblackboard/BBAppStarter.java
+++ src/main/java/co/edu/escuelaing/interactiveblackboard/BBAppStarter.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BBAppStarter {
     public static void main(String[] args){
-        SpringApplication.run(BBAppStarter.class,args);
-      /**
-       * SpringApplication app = new SpringApplication(BBAppStarter.class);
-       * 
-       * app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
-       * app.run(args);
-       */
+       // SpringApplication.run(BBAppStarter.class,args);
+    
+        SpringApplication app = new SpringApplication(BBAppStarter.class);
+        
+       app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+       app.run(args);
+       
     }
     
     static int getPort() {
